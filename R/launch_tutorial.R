2e8cc54eab1f7cfcffdebe3e950f0985e85ebfb1
kimbrianj <kimbrian@umd.edu>
2020-07-20 16:31:28 -0400

    Updates to the launch_tutorial man page.

--- R/launch_tutorial.R
+++ R/launch_tutorial.R
@@ -4,11 +4,12 @@
 #'
 #' @param tutorial character, representing the name of a tutorial to run. Options include: 
 #' 
-#' 1) 'intro-to-r': A basic introduction to how to use R.
+#' 1. 'intro-to-r': A basic introduction to how to use R.
+#' 2. 't-tests': An introduction to doing t-tests in R.
 #'
 #' @return None.
 #' @export
-#'
+#' @md
 #' @examples 
 #' launch_tutorial('intro-to-r')
 launch_tutorial <- function(tutorial){
