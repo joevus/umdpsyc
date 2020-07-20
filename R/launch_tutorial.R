673fc9fa590c60b7020956b86203eb2aa0491fa0
kimbrianj <kimbrian@umd.edu>
2020-07-20 16:05:31 -0400

    Added launch_tutorial function

---
+++ R/launch_tutorial.R
@@ -0,0 +1,15 @@
+#' Launch Tutorials
+#' 
+#' This function is used to launch tutorials. 
+#'
+#' @param tutorial character, representing the name of a tutorial to run. Options include:
+#' 1) 'intro-to-r': A basic introduction to how to use R.
+#'
+#' @return None.
+#' @export
+#'
+#' @examples 
+#' launch_tutorial('intro-to-r')
+launch_tutorial <- function(tutorial){
+  learnr::run_tutorial(tutorial, 'umdpsyc')
+}
