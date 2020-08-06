2f14c978f89774ef0fa3568e51432e0783c7ef02
kimbrianj <kimbrian@umd.edu>
2020-08-06 16:03:25 -0400

    Updates to add more tutorials and add data.

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' Production and farm value of maple products in Canada
+#'
+#' @source Generated based on published results from various studies.
+#' @format A data frame with columns:
+#' \describe{
+#'  \item{score}{A value between 0 and 100}
+#'  \item{hrs_sleep_per_week}{Hours slept per week.}
+#'  \item{motivation}{Student motivation. Likert scale from 1 to 10.}
+#'  \item{note_mode}{The method of note-taking. 1 = Paper, 2 = Computer.}
+#' }
+#' @examples
+#' \dontrun{
+#'  exam_scores
+#' }
+"exam_scores"
