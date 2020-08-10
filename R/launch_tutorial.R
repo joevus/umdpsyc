f9b3a8481f4f3126fcb8c0c0000253887c99bb86
kimbrianj <kimbrian@umd.edu>
2020-08-10 16:22:47 -0400

    Updates to data frames and t-tests tutorials.

--- R/launch_tutorial.R
+++ R/launch_tutorial.R
@@ -1,16 +1,22 @@
 #' Launch Tutorials
-#' 
-#' This function is used to launch tutorials. 
 #'
-#' @param tutorial character, representing the name of a tutorial to run. Options include: 
-#' 
+#' This function is used to launch tutorials.
+#'
+#' @param tutorial character, representing the name of a tutorial to run. Options include:
+#'
 #' 1. 'intro-to-r': A basic introduction to how to use R.
-#' 2. 't-tests': An introduction to doing t-tests in R.
+#' 2. 'data-frames': An introduction to using data frames in R.
+#' 3. 'categorical-descriptives': Using R to do basic descriptive statistics with categorical data.
+#' 4. 'numerical-descriptives': Using R to do basic descriptive statistics with numerical data.
+#' 5. 'regression': Ordinary least squares regression using R.
+#' 6. 't-tests': An introduction to doing t-tests in R.
+#' 7. 'anova': Using R for ANOVA.
+#' 8.
 #'
 #' @return None.
 #' @export
 #' @md
-#' @examples 
+#' @examples
 #' launch_tutorial('intro-to-r')
 launch_tutorial <- function(tutorial){
   learnr::run_tutorial(tutorial, 'umdpsyc')
