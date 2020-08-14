0383c6368782b0dede617fdfbc86d73cf327296b
kimbrianj <kimbrian@umd.edu>
2020-08-14 16:33:53 -0400

    Updated documentation, added more to the tutorials, updated the exam_scores data.

--- R/data.R
+++ R/data.R
@@ -1,12 +1,15 @@
 #' Exam scores and sleep
 #'
-#' @source Generated based on published results from various studies.
+#' @source An artifical dataset created for demonstrative purposes only. Generated based on published results from various studies.
 #' @format A data frame with columns:
 #' \describe{
-#'  \item{score}{A value between 0 and 100}
-#'  \item{hrs_sleep_per_week}{Hours slept per week.}
-#'  \item{motivation}{Student motivation. Likert scale from 1 to 10.}
+#'  \item{exam1}{Score on the first exam. A value between 0 and 100.}
+#'  \item{motivation}{Self-reported motivation score. Likert scale from 1 to 10.}
 #'  \item{note_mode}{The method of note-taking. 1 = Paper, 2 = Computer.}
+#'  \item{exam2}{Score on the second exam. A value between 0 and 100.}
+#'  \item{anxiety}{Self-reported level of anxiety. 1 = low, 2 = medium, 3 = high.}
+#'  \item{sleep_week}{Minutes of sleep in the week before exam 1.}
+#'  \item{sleep_day}{Minutes of sleep in the day before exam 1.}
 #' }
 #' @examples
 #' \dontrun{
--- R/launch_tutorial.R
+++ R/launch_tutorial.R
@@ -4,14 +4,13 @@
 #'
 #' @param tutorial character, representing the name of a tutorial to run. Options include:
 #'
-#' 1. 'intro-to-r': A basic introduction to how to use R.
-#' 2. 'data-frames': An introduction to using data frames in R.
-#' 3. 'categorical-descriptives': Using R to do basic descriptive statistics with categorical data.
-#' 4. 'numerical-descriptives': Using R to do basic descriptive statistics with numerical data.
-#' 5. 'regression': Ordinary least squares regression using R.
-#' 6. 't-tests': An introduction to doing t-tests in R.
-#' 7. 'anova': Using R for ANOVA.
-#' 8.
+#' 1. `'intro-to-r'`: A basic introduction to how to use R.
+#' 2. `'data-frames'`: An introduction to using data frames in R.
+#' 3. `'categorical-descriptives'`: Using R to do basic descriptive statistics with categorical data.
+#' 4. `'numerical-descriptives'`: Using R to do basic descriptive statistics with numerical data.
+#' 5. `'regression'`: Ordinary least squares regression using R.
+#' 6. `'t-tests'`: An introduction to doing t-tests in R.
+#' 7. `'anova'`: Using R for ANOVA.
 #'
 #' @return None.
 #' @export
