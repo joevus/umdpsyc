9b60ec42894d117c25fe546e9faf58a5afd2ad5a
kimbrianj <kimbrian@umd.edu>
2020-08-20 12:00:14 -0400

    Updates to descriptive statistics tutorials.

--- R/launch_tutorial.R
+++ R/launch_tutorial.R
@@ -6,8 +6,8 @@
 #'
 #' 1. `'intro-to-r'`: A basic introduction to how to use R.
 #' 2. `'data-frames'`: An introduction to using data frames in R.
-#' 3. `'categorical-descriptives'`: Using R to do basic descriptive statistics with categorical data.
-#' 4. `'numerical-descriptives'`: Using R to do basic descriptive statistics with numerical data.
+#' 3. `'numerical-descriptives'`: Using R to do basic descriptive statistics with numerical data.
+#' 4. `'categorical-descriptives'`: Using R to do basic descriptive statistics with categorical data.
 #' 5. `'regression'`: Ordinary least squares regression using R.
 #' 6. `'t-tests'`: An introduction to doing t-tests in R.
 #' 7. `'anova'`: Using R for ANOVA.
